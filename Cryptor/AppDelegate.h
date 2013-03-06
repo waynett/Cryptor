4b7eff6ae70b9dc69fcee479fb28a2a4c321df8b
zhangda <alfaromeo.dev@gmail.com>
2013-03-06 15:51:59 +0800

    init

---
+++ Cryptor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cryptor
+//
+//  Created by zhang da on 13-3-6.
+//  Copyright (c) 2013年 zhang da. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
